b32aff65814f367c3bd30305162622b9ef77e71d
Dmitry Popov <dimoonster@gmail.com>
2019-01-20 20:16:09 +0300

    packer cfgs and install scripts

---
+++ packer/scripts/install_ruby.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+## Проверим, что скрипт запускаем от имени root
+MYID=`id -u`
+if test "${MYID}" != '0' ; then
+    echo Need root access. 
+    echo Run $0 use sudo.
+    exit
+fi
+
+## Обновим репозитории и установим ruby, bundler и инструменты для сборки
+apt-get update
+apt-get install -y ruby-full ruby-bundler build-essential
